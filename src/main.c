19f4b3b28c9fa885c943e74b7c320bf5d729bb9c
Breno Queiroz <breno1423@gmail.com>
2020-05-17 15:57:58 -0300

    Test: curses

---
+++ src/main.c
@@ -0,0 +1,49 @@
+#include <curses.h>
+#include <unistd.h>
+
+#define SIZE 5
+
+void drawGraph(WINDOW *win);
+
+int main(int argc, char *argv[]) {
+	//--- Inicializa a janela ---//
+	initscr();
+	noecho();
+	curs_set(FALSE);
+
+	//--- Cria janela do jogo ---//
+	int maxX=0, maxY=0;
+	// Recebe tamanho da janela
+	getmaxyx(stdscr, maxY, maxX);
+	// Cria janela (posY, posX, offsetY, offsetX)
+	WINDOW *game  = newwin(SIZE, SIZE, maxY/2-SIZE/2, maxX/2-SIZE/2);
+	refresh();
+
+	// Desenha grafo
+	drawGraph(game);
+
+	usleep(1*1000000);	
+	// Destroi a janela
+	endwin();
+
+	return 0;
+}
+
+void drawGraph(WINDOW *win)
+{
+	int x,y;
+	// Limpa janela
+	wclear(win);
+
+	// Desenha grafo no centro da janela
+	for(x=0;x<SIZE;x++)
+	{
+		for(y=0;y<SIZE;y++)
+		{
+			mvwprintw(win, y, x, "0");
+		}
+	}
+	
+	// Atualiza a janela
+	wrefresh(win);
+}
